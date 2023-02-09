1b54ce70dd35c0d640c3cce4fdac3f8204a15258
Wh_Frost <whfrost63@gmail.com>
2023-02-09 21:57:06 +0400

    Настроит окружение

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import './global.scss';
+import Button from './button/Button';
+
+const App = () => {
+	return (
+		<div className='app'>
+			asdfasdf
+			<Button />
+		</div>
+	);
+};
+
+export default App;
